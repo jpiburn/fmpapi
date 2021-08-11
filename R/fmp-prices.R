d3bc125ee1b0c895d4d82b36c7915a689c896dde
Piburn <pij@ornl.gov>
2021-08-10 20:13:43 -0400

    add ... to price

--- R/fmp-prices.R
+++ R/fmp-prices.R
@@ -32,6 +32,7 @@ fmp_prices <- function(symbol, interval = c('1min', '5min', '15min', '30min', '1
 #' @param start_date `character`. ISO-8601 formatted date. For example, `'2020-05-25'`
 #' @param end_date `character`. ISO-8601 formatted date. For example, `'2020-06-25'`
 #' @param last_n  `numeric`. An alternative to `start_date` and `end_date`. return the last `n` days
+#' @param ... additional parameters
 #'
 #' @return a [tibble][tibble::tibble-package] of quotes of requested securities
 #' @examples
@@ -42,7 +43,7 @@ fmp_prices <- function(symbol, interval = c('1min', '5min', '15min', '30min', '1
 #' }
 #'
 #' @export
-fmp_daily_prices <- function(symbol, start_date = NULL, end_date = NULL, last_n = NULL) {
+fmp_daily_prices <- function(symbol, start_date = NULL, end_date = NULL, last_n = NULL, ...) {
   endpoint <- 'historical-price-full'
 
   query_list <- list(
@@ -57,7 +58,7 @@ fmp_daily_prices <- function(symbol, start_date = NULL, end_date = NULL, last_n 
     query_list$to  <- NULL
   }
 
-  request_urls <- build_request_urls(symbol, endpoint = endpoint, query_list = query_list)
+  request_urls <- build_request_urls(symbol, endpoint = endpoint, query_list = query_list, ...)
   d <- get_request_content(request_urls, endpoint = endpoint)
 
   d
