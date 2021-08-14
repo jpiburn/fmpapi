57574a7ede820035b4e1873e79c416c2c395d72c
Piburn <pij@ornl.gov>
2021-08-14 14:24:19 -0400

    refactor build_request_urls for api v4 and cloud server

--- R/build-api-request.R
+++ R/build-api-request.R
@@ -27,59 +27,121 @@ build_request_urls <- function(path_params, api_version = 'v3', endpoint, query_
       'To obtain an API key please visit https://financialmodelingprep.com/developer/docs/pricing/',
       "\nOnce obtained, you can call `fmp::fmp_api_key('my_api_key')` to save your key to .Renviron file.",
       "\nWhen first installed, you will need to reload your .Renviron file by either restarting R or running `readRenviron('~/.Renviron')`"
-      )
+    )
 
     warning(warning_message)
     apikey <- 'demo'
   }
 
-  # determine if base_urls -----
+  url_path_stem <- paste0(c('api', api_version, endpoint), collapse = "/")
+  if (!is.null(path_params)) url_path_stem <- paste(url_path_stem, path_params, sep = "/")
+
+  null_queries <- sapply(query_list, is.null)
+
+  if (all(null_queries)) url_query_stem <- paste0("apikey=", apikey)
+  else{
+    query_list <- query_list[!null_queries]
+    url_df <- expand.grid(query_list)
+    query_names <- names(url_df)
+    for (i in 1:ncol(url_df)) {
+      url_df[ , query_names[i]] <- paste0(query_names[i], "=", url_df[ , query_names[i]])
+    }
+    url_df$apikey <- paste0("apikey=", apikey)
+    url_query_stem <- apply(url_df, 1, paste0, collapse = "&")
+  }
+  endpoint_urls_df <- tidyr::expand_grid(path = url_path_stem, query = url_query_stem)
+
+  # determine base_urls -----
   if (is.null(base_url)) {
-   if (cloud_access) base_url <- get_base_url('all')
-   else base_url <- get_base_url('fmp')
+    if (cloud_access) base_url <- get_base_url('all')
+    else base_url <- get_base_url('fmp')
   }
 
-  # if access to only one server (e.g. no fmpcloud access) -----
-  if (length(base_url) == 1) {
-    if (is.null(path_params)) {
-      request_urls <- httr::modify_url(base_url,
-                                       path =  c('api', api_version, endpoint),
-                                       query = as.list(c(query_list, apikey = apikey))
-                                       )
-    }
-    else {
-    request_urls <- purrr::map_chr(path_params,
-                   ~ httr::modify_url(base_url,
-                                      path =  c('api', api_version, endpoint, .x),
-                                      query = as.list(c(query_list, apikey = apikey))
-                                      )
-                                   )
-    }
-  }
-  # if access to multiple servers (e.g. has fmpcloud access) -----
-  else {
-    if (is.null(path_params)) {
-      request_urls <- httr::modify_url(sample(base_url, 1), # spread calls across all servers
-                                       path =  c('api', api_version, endpoint),
-                                       query = as.list(c(query_list, apikey = apikey))
-      )
-    }
-    else {
+  n_params <- nrow(endpoint_urls_df)
+  n_urls <- length(base_url)
+  url_base_stem <- rep(base_url, ceiling(n_params / n_urls))[1:n_params]
+  endpoint_urls_df$base_url <- url_base_stem
 
-      n_params <- length(path_params)
-      n_urls <- length(base_url)
-      url_params <- rep(base_url, ceiling(n_params / n_urls))[1:n_params]
-      request_urls <- purrr::map2_chr(path_params, url_params,
-                                     ~ httr::modify_url(.y,
-                                                        path =  c('api', api_version, endpoint, .x),
-                                                        query = as.list(c(query_list, apikey = apikey))
-                                     )
-      )
-    }
-  }
+  endpoint_urls_df$request_url <- glue::glue_data(endpoint_urls_df, '{base_url}/{path}?{query}')
+  request_urls <- endpoint_urls_df$request_url
 
   request_urls
 }
+
+
+
+#'
+#'
+#' #' @noRd
+#' build_request_urls <- function(path_params, api_version = 'v3', endpoint, query_list = NULL, base_url = NULL) {
+#'
+#'   if (Sys.getenv('FMP_API_KEY') != '')  {
+#'
+#'     apikey <- Sys.getenv('FMP_API_KEY')
+#'     cloud_access <- as.logical(Sys.getenv('FMP_CLOUD_ACCESS'))
+#'     if(is.na(cloud_access)) cloud_access <- FALSE
+#'
+#'   } else {
+#'
+#'     warning_message <- glue::glue(
+#'       'An API Key is required to use the Financial Modeling Prep API and fmp package.',
+#'       'No API Key was found and a limited use key of "demo" is being used.',
+#'       'To obtain an API key please visit https://financialmodelingprep.com/developer/docs/pricing/',
+#'       "\nOnce obtained, you can call `fmp::fmp_api_key('my_api_key')` to save your key to .Renviron file.",
+#'       "\nWhen first installed, you will need to reload your .Renviron file by either restarting R or running `readRenviron('~/.Renviron')`"
+#'       )
+#'
+#'     warning(warning_message)
+#'     apikey <- 'demo'
+#'   }
+#'
+#'   # determine if base_urls -----
+#'   if (is.null(base_url)) {
+#'    if (cloud_access) base_url <- get_base_url('all')
+#'    else base_url <- get_base_url('fmp')
+#'   }
+#'
+#'   # if access to only one server (e.g. no fmpcloud access) -----
+#'   if (length(base_url) == 1) {
+#'     if (is.null(path_params)) {
+#'       request_urls <- httr::modify_url(base_url,
+#'                                        path =  c('api', api_version, endpoint),
+#'                                        query = as.list(c(query_list, apikey = apikey))
+#'                                        )
+#'     }
+#'     else {
+#'     request_urls <- purrr::map_chr(path_params,
+#'                    ~ httr::modify_url(base_url,
+#'                                       path =  c('api', api_version, endpoint, .x),
+#'                                       query = as.list(c(query_list, apikey = apikey))
+#'                                       )
+#'                                    )
+#'     }
+#'   }
+#'   # if access to multiple servers (e.g. has fmpcloud access) -----
+#'   else {
+#'     if (is.null(path_params)) {
+#'       request_urls <- httr::modify_url(sample(base_url, 1), # spread calls across all servers
+#'                                        path =  c('api', api_version, endpoint),
+#'                                        query = as.list(c(query_list, apikey = apikey))
+#'       )
+#'     }
+#'     else {
+#'
+#'       n_params <- length(path_params)
+#'       n_urls <- length(base_url)
+#'       url_params <- rep(base_url, ceiling(n_params / n_urls))[1:n_params]
+#'       request_urls <- purrr::map2_chr(path_params, url_params,
+#'                                      ~ httr::modify_url(.y,
+#'                                                         path =  c('api', api_version, endpoint, .x),
+#'                                                         query = as.list(c(query_list, apikey = apikey))
+#'                                      )
+#'       )
+#'     }
+#'   }
+#'
+#'   request_urls
+#' }
 
 #' @noRd
 get_request_content <- function(request_urls, ...) {
